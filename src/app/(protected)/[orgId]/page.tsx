721fa781eb8cf1cd837ec4db1d4544250fbf174e
Tushar Mistry <mistry.tushar98@gmail.com>
2024-02-16 20:46:09 +0530

    chore(repo): eslint, lint-staged, commitlint, vscode setup

--- src/app/(protected)/[orgId]/page.tsx
+++ src/app/(protected)/[orgId]/page.tsx
@@ -1,30 +1,30 @@
-import { redirect } from "next/navigation"
-import { getServerSession } from "next-auth"
+import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth";
 
-import { prisma } from "@/lib/prisma"
-import { nextAuthOptions } from "@/lib/next-auth";
 import { Routes } from "@/lib/constants/routes";
+import { nextAuthOptions } from "@/lib/next-auth";
+import { prisma } from "@/lib/prisma";
 
 export default async function Dashboard() {
-  const session = await getServerSession(nextAuthOptions)
+  const session = await getServerSession(nextAuthOptions);
 
   if (!session?.user?.id) {
-    redirect("/auth/signin")
+    redirect("/auth/signin");
   }
 
   const org = await prisma.organizations.findFirst({
     where: {
       user_id: session?.user?.id,
     },
-  })
+  });
 
   const orgUser = await prisma.orgUsers.findFirst({
     where: { user_id: session?.user?.id },
     include: { org: true },
-  })
+  });
 
   if (org || orgUser) {
-    return redirect(`/${org?.name ?? orgUser?.org?.name}${Routes.HOME}`)
+    return redirect(`/${org?.name ?? orgUser?.org?.name}${Routes.HOME}`);
   }
-  return redirect("/auth/signup")
+  return redirect("/auth/signup");
 }
