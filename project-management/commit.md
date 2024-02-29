b7261e54139718bc8ab858a3659fdd80a38867fc
Tushar Mistry <mistry.tushar98@gmail.com>
2024-02-29 11:54:10 +0530

    fix(dashboard): redirect issue
    the / route was affecting because it redirected on itself again and again

---
+++ project-management/commit.md
@@ -0,0 +1,3 @@
+## How to commit on the project
+
+- Following conventional commit 
--- src/app/(protected)/[orgId]/page.tsx
+++ src/app/(protected)/[orgId]/page.tsx
@@ -1,30 +1,3 @@
-import { redirect } from "next/navigation";
-import { getServerSession } from "next-auth";
-
-import { Routes } from "@/lib/constants/routes";
-import { nextAuthOptions } from "@/lib/next-auth";
-import { prisma } from "@/lib/prisma";
-
 export default async function Dashboard() {
-  const session = await getServerSession(nextAuthOptions);
-
-  if (!session?.user?.id) {
-    redirect("/auth/signin");
-  }
-
-  const org = await prisma.organizations.findFirst({
-    where: {
-      user_id: session?.user?.id,
-    },
-  });
-
-  const orgUser = await prisma.orgUsers.findFirst({
-    where: { user_id: session?.user?.id },
-    include: { org: true },
-  });
-
-  if (org || orgUser) {
-    return redirect(`/${org?.name ?? orgUser?.org?.name}${Routes.HOME}`);
-  }
-  return redirect("/auth/signup");
+  return <>Dashboard</>;
 }
